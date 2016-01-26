6c6980f00ae4987ac36ae817c3e683156f77af7e
josephtoles <jbtoles@gmail.com>
2016-01-27 01:01:11 +0800

    Got update loop working

--- src/main/java/com/slightlyrobot/app/AccelerometerReadoutFragment.java
+++ src/main/java/com/slightlyrobot/app/AccelerometerReadoutFragment.java
@@ -2,15 +2,35 @@ package com.slightlyrobot.app;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.lang.Runnable;
+
 import com.slightlyrobot.app.AccelerometerSamplerService;
 import com.slightlyrobot.app.RawRecordsDatabaseOpenHelper;
 
 public class AccelerometerReadoutFragment extends Fragment {
+
+    private Handler handler = new Handler();
+    private final int interval = 1000;
+    private RawRecordsDatabaseOpenHelper db = new RawRecordsDatabaseOpenHelper(this.getActivity());
+    private int loopCount = 0;
+
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            TextView textView = (TextView) getView().findViewById(R.id.readout_text_view);
+            double[] latestValues = db.getLastValues();
+            textView.setText(latestValues[0] + ", " + 
+                             latestValues[1] + ", " + 
+                             latestValues[2]);
+            handler.postDelayed(runnable, interval);
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,13 +42,7 @@ public class AccelerometerReadoutFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        
-        TextView textView = (TextView) getView().findViewById(R.id.readout_text_view);
-        textView.setText("updated");
-        
-        RawRecordsDatabaseOpenHelper db = new RawRecordsDatabaseOpenHelper(this.getActivity());
-        double[] latestValues = db.getLastValues();
-        textView.setText("" + latestValues[0] + ", " + latestValues[1] + ", " + latestValues[2]);
+        handler.postDelayed(runnable, 0);
     }
 
     @Override
